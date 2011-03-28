dfa14a26fd7a3d12e536c5ab97360b84a3fde53d
Carl Hörberg <carl.hoerberg@gmail.com>
2011-03-28 23:07:20 +0200

    gist replace fix

--- lib/app.rb
+++ lib/app.rb
@@ -28,7 +28,7 @@ helpers do
 	end
 
 	def replace_gist(html)
-		html.gsub(/(http[s]?:\/\/gist.github.com\/[\d]+)/,
+		html.gsub(/(http[s]?:\/\/gist.github.com\/[\d]+) /,
 							'<script src="\1.js"></script>')
 	end
 end
